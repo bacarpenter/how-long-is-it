de50ed2c941f667b798ef79924cb1d9fad8996ba
bacarpenter <bacarpenter04@gmail.com>
2020-05-02 15:44:06 -0400

    Added Grams and Newtons, functionilty is mainly done! :)

--- app.js
+++ app.js
@@ -15,10 +15,9 @@ function getFormData() {
     else if (Units == "Grams") {
         loadVisualsG();
     }
-    else if (Units == Newtons) {
+    else if (Units == "Newtons") {
         loadVisualsN();
     }
-
 }
 
 function loadVisualsKM() {
@@ -36,7 +35,7 @@ function loadVisualsKM() {
         var Time = Math.round(TimeUn);
         document.getElementById("InfoHeader").innerHTML = "It would take you about " + Time + " miuntes to bike " + numberOf + " kilometers";
     }
-return;
+    return;
 }
 
 function loadVisualsL() {
@@ -50,7 +49,7 @@ function loadVisualsL() {
     else if (n == 2) {
         //Lieters to fill a olympic swimming pool. ammount from https://www.patagoniaalliance.org/wp-content/uploads/2014/08/How-much-water-does-an-Olympic-sized-swimming-pool-hold.pdf
         var TimeUn = numberOf / 2500000;
-        var Time =  TimeUn;
+        var Time = TimeUn;
         document.getElementById("InfoHeader").innerHTML = numberOf + " lieters would fill about " + Time + " olympic swimming pools";
     }
     else {
@@ -60,6 +59,53 @@ function loadVisualsL() {
         document.getElementById("InfoHeader").innerHTML = numberOf + " lieters would fill about " + Time + " milk jugs (1 gal)";
     }
 }
+
+function loadVisualsG() {
+    var n = getRandomInt(3);
+    if (n == 1) {
+        //Chaey Taho. Mass from https://media.gm.com/media/us/en/chevrolet/vehicles/tahoe/2018.tab1.html 4 wheel drive modle, converted with google converter.
+        var TimeUn = numberOf / 2554000;
+        var Time = TimeUn;
+        document.getElementById("InfoHeader").innerHTML = numberOf + " grams is about " + Time + "  Chevrolet Tahoes (2018, 4 wheel drive)";
+    }
+    else if (n == 2) {
+        //gallon of watter. Mass from https://study.com/academy/answer/how-much-does-one-gallon-of-water-weigh.html
+        var TimeUn = numberOf / 3785;
+        var Time = Math.round(TimeUn);
+        document.getElementById("InfoHeader").innerHTML = numberOf + " grams is about " + Time + " gallons of watter.";
+    }
+    else {
+        //iPhone X. Mass from https://www.google.com/search?q=iphone+10+weight+in+grams&rlz=1C5CHFA_enUS883US883&oq=iPhone+10+weight&aqs=chrome.1.0l8.6890j1j7&sourceid=chrome&ie=UTF-8
+        var TimeUn = numberOf / 174;
+        var Time = Math.round(TimeUn);
+        document.getElementById("InfoHeader").innerHTML = numberOf + " grams is about " + Time + " iPhone Xs.";
+    }
+}
+
+function loadVisualsN() {
+    var n = getRandomInt(3);
+    if (n == 1) {
+        //force produced by the first stage of the falcon9, at sea leaval. From: https://www.spacex.com/falcon9
+        var TimeUn = numberOf / (7607 * 1000);
+        var Time = TimeUn;
+        document.getElementById("InfoHeader").innerHTML = numberOf + " newtons is " + Time + " times the force produced by the Falcon 9's first stage rocket"
+    }
+    else if (n == 2) {
+        //force required to lift a bolwing ball. Math from https://www.calculatorsoup.com/calculators/physics/force.php 
+        var TimeUn = numberOf / 4.14765;
+        var Time = Math.round(TimeUn);
+        document.getElementById("InfoHeader").innerHTML = numberOf + " newtons could lift " + Time + " ten lbs bollowing balls at an acceleration of 3ft/s"
+
+    }
+    else {
+        //force required to break an egg. From https://www.njeffersonnews.com/slate-the-physics-of-cracking-an-egg/article_aee37af3-6d76-54e0-bdad-7f799d5c1136.html. Converted with google copnverter
+        var TimeUn = numberOf / 24.4652;
+        var Time = Math.round(TimeUn);
+        document.getElementById("InfoHeader").innerHTML = numberOf + " newtons could break " + Time + " eggs."
+    }
+}
+
+
 
 //From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(max) {
