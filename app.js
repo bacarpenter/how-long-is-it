62fe5791ef636a2df5dd7a127a3ca5704cecee45
bacarpenter <bacarpenter04@gmail.com>
2020-05-02 14:35:12 -0400

    Added liter support. This marks the 1/2 way point for functinality

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var numberOf;
 var Units;
 
-
 //Function to get the data from user
 function getFormData() {
     numberOf = document.getElementById("NumberOf").value;
@@ -40,7 +39,29 @@ function loadVisualsKM() {
 return;
 }
 
-//From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random:
+function loadVisualsL() {
+    var n = getRandomInt(3);
+    if (n == 1) {
+        //Lieters to fill a red party cup. amount from https://www.solocup.com/products/18-oz-plastic-party-cup/
+        var TimeUn = numberOf / 0.532324;
+        var Time = Math.round(TimeUn);
+        document.getElementById("InfoHeader").innerHTML = numberOf + " lieters would fill about " + Time + " standard party cups";
+    }
+    else if (n == 2) {
+        //Lieters to fill a olympic swimming pool. ammount from https://www.patagoniaalliance.org/wp-content/uploads/2014/08/How-much-water-does-an-Olympic-sized-swimming-pool-hold.pdf
+        var TimeUn = numberOf / 2500000;
+        var Time =  TimeUn;
+        document.getElementById("InfoHeader").innerHTML = numberOf + " lieters would fill about " + Time + " olympic swimming pools";
+    }
+    else {
+        //Lieters to fill a milk jug (1 gallon). ammount from google converter
+        var TimeUn = numberOf / 4.54609;
+        var Time = Math.round(TimeUn);
+        document.getElementById("InfoHeader").innerHTML = numberOf + " lieters would fill about " + Time + " milk jugs (1 gal)";
+    }
+}
+
+//From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
